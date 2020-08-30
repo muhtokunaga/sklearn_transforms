24e65063cf03c5d1e122355b23b5cf65c4e0d2f0
Murilo Tokunaga <murilo.tokunaga@yahoo.com.br>
2020-08-30 13:44:37 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,43 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+    
+class FillNotaGo(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        data['NOTA_GO'] = np.where(data['REPROVACOES_GO'] > 0, data['NOTA_GO'].fillna(0), data['NOTA_GO'])
+        data['NOTA_GO'].fillna(data['NOTA_GO'].median(), inplace = True)
+        return data
+
+    
+class FillIngles(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+        data['INGLES'].fillna(0, inplace = True)
+        return data
+
+    
+class PerfilTransform(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()#Aplicando tranform no perfil
+        data['PERFIL'] = data['PERFIL'].map( {'EXATAS': 0, 'HUMANAS': 1, 'DIFICULDADE': 2, 'MUITO_BOM': 3, 'EXCELENTE': 4} )
+        return data
